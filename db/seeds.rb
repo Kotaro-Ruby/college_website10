97d6665db6b9619920c2a1e89d6eee5b001e6cbd
{kotaro} <{t.kotaro.0222@gmail.com}>
2025-05-21 13:03:18 +0900

    edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,3 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-ruby script/export_colleges_to_seeds.rb
-cat db/seeds_data.rb >> db/seeds.rb
